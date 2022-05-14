10ca89afeb0004165f3a394c37a7b30e8ccd7f3c
MatthewGagnard <mrgagnard@gmail.com>
2022-05-14 15:27:27 -0500

    deploy

--- public/scripts/api-tester.js
+++ public/scripts/api-tester.js
@@ -1,8 +1,8 @@
 async function testReadAll(){
     const config = new Object();
     config.method = "POST";
-    const response = await fetch("http://localhost:80/Player?NAME=Vensin&HP=45&AC=14&WIS=20", config);
+    const response = await fetch("https://monster-stat-api.herokuapp.com/Player?NAME=Vensin&HP=45&AC=14&WIS=20", config);
     const data = await response.json()
-    document.body.innerHTML += `<p>${JSON.stringify(data)}</p>`
+    document.getElementById('testSpace').innerHTML += `<p>${JSON.stringify(data)}</p>`
     }
     
